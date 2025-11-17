f25d74ac034165eabc7029216cab0c73c0b698c3
lex-carey <alexisacarey@gmail.com>
2025-11-17 18:54:11 -0500

    removed passing texture to DrawSprite by ref

--- src/shader/spriteRenderer.cpp
+++ src/shader/spriteRenderer.cpp
@@ -12,7 +12,7 @@ SpriteRenderer::~SpriteRenderer()
     glDeleteVertexArrays(1, &this->quadVAO);
 }
 
-void SpriteRenderer::DrawSprite(Texture2D &texture, glm::vec2 position, glm::vec2 size, float rotate, glm::vec3 color)
+void SpriteRenderer::DrawSprite(Texture2D texture, glm::vec2 position, glm::vec2 size, float rotate, glm::vec3 color)
 {
     // prepare transformations
     this->shader.use();
--- src/shader/spriteRenderer.h
+++ src/shader/spriteRenderer.h
@@ -9,7 +9,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include "Texture.h"
+#include "texture.h"
 #include "shader.h"
 
 
@@ -21,7 +21,7 @@ public:
     // Destructor
     ~SpriteRenderer();
     // Renders a defined quad textured with given sprite
-    void DrawSprite(Texture2D &texture, glm::vec2 position, glm::vec2 size = glm::vec2(10.0f, 10.0f), float rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f));
+    void DrawSprite(Texture2D texture, glm::vec2 position, glm::vec2 size = glm::vec2(10.0f, 10.0f), float rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f));
 private:
     // Render state
     Shader shader;
